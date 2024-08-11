80af0afeddff711dfe0e5c1909d906fcd4eeda37
zhiguang <zhiguang53846@gmail.com>
2024-08-11 18:21:42 -0400

    initial commit of etchasketch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let size = 16;
-const containerSide = 1000;
-const divSize = containerSide/size;
+let containerSide = 1000;
+let divSize = containerSide/size;
 
 const container = document.querySelector('.container');
 container.style.maxWidth = containerSide+'px';
@@ -12,10 +12,13 @@ const body = document.querySelector('body');
 
 const button = document.createElement('button');
 button.classList.add('reset');
-button.position = 'absolute';
-button.top = '50px';
-button.left = containerSide/2+'px';
 body.appendChild(button);
+body.insertBefore(button,container);
+
+button.style.position = 'absolute';
+button.style.top = '50px';
+button.style.left = containerSide+'px';
+button.textContent = 'Reset';
 
 for (let i = 0;i < size;i++)
 {
@@ -25,7 +28,7 @@ for (let i = 0;i < size;i++)
     }
 }
 
-const grid = document.querySelectorAll('.grid');
+let grid = document.querySelectorAll('.grid');
 
 for (let i = 0;i< grid.length; i++)
 {
@@ -44,3 +47,45 @@ container.style.flexDirection = 'column';
 container.style.justifyContent = 'center';
 container.style.flexWrap = 'wrap';
 container.style.alignItems = 'center';
+
+button.addEventListener('click', reset);
+
+function reset(){
+    do{
+        size = parseInt(prompt('Please enter a number between 1 and 100'));
+    }while (size > 100 || size < 1);
+
+     for (let i = 0;i< grid.length; i++)
+        {
+                container.removeChild(grid[i]);
+        }
+
+        divSize = containerSide/size;
+
+        drawBoard();
+}
+
+function drawBoard(){
+
+    for (let i = 0;i < size;i++)
+        {
+            for (let j = 0;j < size;j++)
+            {
+                container.innerHTML += `<div class="grid"></div>`;
+            }
+        }
+
+        grid = document.querySelectorAll('.grid');
+
+    for (let i = 0;i< grid.length; i++)
+        {
+            grid[i].style.border = '1px solid black';
+            grid[i].style.boxSizing= 'border-box';
+            grid[i].style.width = divSize+'px';
+            grid[i].style.height = divSize+'px';
+            grid[i].style.display='flex';
+            grid[i].addEventListener('mouseover',function (e){
+                e.currentTarget.style.backgroundColor = 'black';
+            });
+        }
+}
